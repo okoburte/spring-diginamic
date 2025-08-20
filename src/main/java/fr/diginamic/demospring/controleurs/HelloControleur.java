b8b8066e72bb72fb4d8117e6403f86ce476ccb05
okoburte <96640848+okoburte@users.noreply.github.com>
2025-08-20 12:03:37 +0200

    tp2

--- src/main/java/fr/diginamic/demospring/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/demospring/controleurs/HelloControleur.java
@@ -1,5 +1,7 @@
 package fr.diginamic.demospring.controleurs;
 
+import fr.diginamic.demospring.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloControleur {
 
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping
     public String direHello(){
-        return "Hello World";
+        return helloService.salutation();
     }
 }
